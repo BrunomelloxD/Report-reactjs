155dfa1d3b302e92422ec413318113a46932a68d
Bruno Mello <brunomellogithub@gmail.com>
2023-12-17 17:41:22 -0300

    Fixing some condiction on login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -11,6 +11,7 @@ const Login = () => {
 
     const [email, setEmail] = useState<string>('')
     const [password, setPassword] = useState<string>('')
+    const [wrongCredentials, setWrongCredentials] = useState<boolean>(false)
 
     const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setEmail(event.target.value)
@@ -36,9 +37,9 @@ const Login = () => {
                 localStorage.setItem('email', response.data.user.email)
                 localStorage.setItem('token', response.data.access_token)
                 navigate('/reports')
-                console.log(response.data)
             }
         } catch (error) {
+            setWrongCredentials(true)
             console.error('Erro ao fazer login:', error)
         }
     }
@@ -60,10 +61,21 @@ const Login = () => {
                                 type="email"
                                 value={email}
                                 onChange={handleEmailChange}
-                                className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-dark outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-dark  placeholder-shown:border-t-dark focus:border-2 focus:border-dark focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-dark"
+                                className={`peer h-full w-full rounded-md border  border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal  outline outline-0 transition-all placeholder-shown:border ${
+                                    wrongCredentials
+                                        ? 'placeholder-shown:border-red-400 focus:border-red-400 disabled:bg-red-400 text-red-400 placeholder-shown:border-t-red-400 border-red-300'
+                                        : 'placeholder-shown:border-dark focus:border-dark disabled:bg-dark text-dark placeholder-shown:border-t-dark border-blue-gray-200'
+                                }  focus:border-2 focus:border-t-transparent focus:outline-0 disabled:border-0`}
                                 placeholder=" "
                             />
-                            <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[12px] peer-focus:leading-tight peer-focus:text-dark-black peer-focus:font-bold peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-dark peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-dark peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-dark">
+                            <label
+                                className={`before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight  transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l  before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1]  peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[12px] 
+                                 peer-focus:leading-tight ${
+                                     wrongCredentials
+                                         ? 'peer-focus:before:!border-red-400 peer-focus:after:!border-red-400 peer-disabled:peer-placeholder-shown:text-red-400 peer-focus:text-red-500 text-red before:border-red-300 after:border-red-300 peer-placeholder-shown:text-red-400 text-red-400'
+                                         : 'peer-focus:before:!border-dark peer-focus:after:!border-dark peer-disabled:peer-placeholder-shown:text-dark peer-focus:text-dark-black text-blue-dark-black before:border-blue-gray-200 after:border-blue-gray-200 peer-placeholder-shown:text-blue-gray-500 text-gray-500'
+                                 }  peer-focus:font-bold peer-focus:before:border-t-2 peer-focus:before:border-l-2  peer-focus:after:border-t-2 peer-focus:after:border-r-2  peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent `}
+                            >
                                 Email
                             </label>
                         </div>
@@ -73,10 +85,21 @@ const Login = () => {
                                 type="password"
                                 value={password}
                                 onChange={handlePasswordChange}
-                                className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-dark outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-dark  placeholder-shown:border-t-dark focus:border-2 focus:border-dark focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-dark"
+                                className={`peer h-full w-full rounded-md border  border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal  outline outline-0 transition-all placeholder-shown:border ${
+                                    wrongCredentials
+                                        ? 'placeholder-shown:border-red-400 focus:border-red-400 disabled:bg-red-400 text-red-400 placeholder-shown:border-t-red-400 border-red-300'
+                                        : 'placeholder-shown:border-dark focus:border-dark disabled:bg-dark text-dark placeholder-shown:border-t-dark border-blue-gray-200'
+                                }  focus:border-2 focus:border-t-transparent focus:outline-0 disabled:border-0`}
                                 placeholder=" "
                             />
-                            <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[12px] peer-focus:leading-tight peer-focus:text-dark-black peer-focus:font-bold peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-dark peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-dark peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-dark">
+                            <label
+                                className={`before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight  transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l  before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1]  peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[12px] 
+                                 peer-focus:leading-tight ${
+                                     wrongCredentials
+                                         ? 'peer-focus:before:!border-red-400 peer-focus:after:!border-red-400 peer-disabled:peer-placeholder-shown:text-red-400 peer-focus:text-red-500 text-red before:border-red-300 after:border-red-300 peer-placeholder-shown:text-red-400 text-red-400'
+                                         : 'peer-focus:before:!border-dark peer-focus:after:!border-dark peer-disabled:peer-placeholder-shown:text-dark peer-focus:text-dark-black text-blue-dark-black before:border-blue-gray-200 after:border-blue-gray-200 peer-placeholder-shown:text-blue-gray-500 text-gray-500'
+                                 }  peer-focus:font-bold peer-focus:before:border-t-2 peer-focus:before:border-l-2  peer-focus:after:border-t-2 peer-focus:after:border-r-2  peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent `}
+                            >
                                 Password
                             </label>
                         </div>
